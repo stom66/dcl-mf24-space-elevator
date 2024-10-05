bb66bc88312f9d537dc438da95882152ca7e8f74
stom <tomsteventon@gmail.com>
2024-10-05 02:19:03 +0100

    fix: sdk7 fixes

--- dcl-scene/src/index.ts
+++ dcl-scene/src/index.ts
@@ -7,15 +7,12 @@
 //
 // Note the 180 rotation, this is so that position in Blender align with positions in DCL (with Y and Z swapped)
 
-import { Settings } from "./_settings";
+//import { Settings } from './_settings'
 
-//import * as utils from "@dcl/ecs-scene-utils";
-//import * as ui from "@dcl/ui-scene-utils";
-//import * as dcldash from "dcldash";
+import { engine, GltfContainer, Transform } from '@dcl/sdk/ecs'
+import { Quaternion, Vector3 } from '@dcl/sdk/math'
 
-const _scene = new Entity("_scene");
-_scene.addComponent(Settings.SCENE_TRANSFORM);
-engine.addEntity(_scene);
+//import { movePlayerTo } from '~system/RestrictedActions'
 
 // ███████╗██╗  ██╗ █████╗ ███╗   ███╗██████╗ ██╗     ███████╗     █████╗ ███████╗███████╗███████╗████████╗
 // ██╔════╝╚██╗██╔╝██╔══██╗████╗ ████║██╔══██╗██║     ██╔════╝    ██╔══██╗██╔════╝██╔════╝██╔════╝╚══██╔══╝
@@ -25,35 +22,21 @@ engine.addEntity(_scene);
 // ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝     ╚══════╝╚══════╝    ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝   ╚═╝
 //
 
-let arm = new Entity();
-arm.addComponent(new GLTFShape("models/armature.gltf"));
-arm.addComponent(
-	new Transform({
-		position: new Vector3(0, 0, 0),
-		scale: new Vector3(1, 1, 1),
-		rotation: Quaternion.Euler(0, 0, 0),
-	})
-);
-arm.setParent(_scene);
+// Armature
+/* let arm = engine.addEntity()
+Transform.create(arm, {
+	position: Vector3.create(0, 0, 0),
+	scale: Vector3.create(1, 1, 1),
+	rotation: Quaternion.fromEulerDegrees(0, 180, 0)
+})
+GltfContainer.create(arm, { src: 'models/armature.gltf' }) */
 
-/* let spaceElevator = new Entity();
-spaceElevator.addComponent(new GLTFShape("models/space-elevator.gltf"));
-spaceElevator.addComponent(
-	new Transform({
-		position: new Vector3(0, 0, 0),
-		scale: new Vector3(1, 1, 1),
-		rotation: Quaternion.Euler(0, 0, 0),
-	})
-);
-spaceElevator.setParent(_scene); */
+// Elevator
 
-/* let conveyor = new Entity();
-conveyor.addComponent(new GLTFShape("models/conveyor-test.gltf"));
-conveyor.addComponent(
-	new Transform({
-		position: new Vector3(-8, 0, -8),
-		scale: new Vector3(1, 1, 1),
-		rotation: Quaternion.Euler(0, 0, 0),
-	})
-);
-conveyor.setParent(_scene); */
+let elevator = engine.addEntity()
+Transform.create(elevator, {
+	position: Vector3.create(0, 0, 0),
+	scale: Vector3.create(1, 1, 1),
+	rotation: Quaternion.fromEulerDegrees(0, 180, 0)
+})
+GltfContainer.create(elevator, { src: 'models/space-elevator.gltf' })
