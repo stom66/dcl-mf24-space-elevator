eaa0616bd813dc5e01860e8534a76beda0fa640e
stom <tomsteventon@gmail.com>
2024-10-11 21:17:01 +0100

    cleanup: remove old assets, switch to glb

--- dcl-scene/src/index.ts
+++ dcl-scene/src/index.ts
@@ -5,7 +5,6 @@ import { ui_debug } from './ui'
 import { ReactEcsRenderer } from '@dcl/sdk/react-ecs'
 
 
-
 //  █████╗ ███████╗███████╗███████╗████████╗███████╗
 // ██╔══██╗██╔════╝██╔════╝██╔════╝╚══██╔══╝██╔════╝
 // ███████║███████╗███████╗█████╗     ██║   ███████╗
@@ -15,36 +14,14 @@ import { ReactEcsRenderer } from '@dcl/sdk/react-ecs'
 //
 // Note the 180 rotation, this is so that position in Blender align with positions in DCL (with Y and Z swapped)
 
-const EXT = ".glb" 
-
-
 let spaceStation = engine.addEntity()
 Transform.create(spaceStation, {
 	position: Vector3.create(0, 0, 0),
 	scale   : Vector3.create(1, 1, 1),
 	rotation: Quaternion.fromEulerDegrees(0, 180, 0)
 })
-GltfContainer.create(spaceStation, { src: 'models/space-elevator.gltf' })
+GltfContainer.create(spaceStation, { src: 'models/space-elevator.glb' })
 
-
-
-/* let spaceStationModel = engine.addEntity()
-Transform.create(spaceStationModel, {
-	position: Vector3.create(0, 0, 0),
-	scale   : Vector3.create(1, 1, 1),
-	rotation: Quaternion.fromEulerDegrees(0, 180, 0)
-})
-GltfContainer.create(spaceStationModel, { src: 'models/space-elevator-model' + EXT })
-
-let spaceStationArmature = engine.addEntity()
-Transform.create(spaceStationArmature, {
-	position: Vector3.create(0, 0, 0),
-	scale   : Vector3.create(1, 1, 1),
-	rotation: Quaternion.fromEulerDegrees(0, 180, 0)
-})
-GltfContainer.create(spaceStationArmature, { src: 'models/space-elevator-armature' + EXT })
-
- */
 
 // ██╗   ██╗██╗
 // ██║   ██║██║
